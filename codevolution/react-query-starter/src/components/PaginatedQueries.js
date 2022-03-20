09b57ad73a51e7162553b02d8e5125eeab05b920
Stephen Moran <stephenmoran@live.ie>
2022-03-20 17:06:43 +0000

    React Query Tutorial - 19 - Paginated Queries

---
+++ codevolution/react-query-starter/src/components/PaginatedQueries.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { useQuery } from "react-query"
+import axios from 'axios'
+
+const fetchColors = (pageNumber) => {
+    return axios.get(`http://localhost:3000/colors?_limit=2&_page=${pageNumber}`)
+}
+
+export const PaginatedQueries = () => {
+    const [pageNumber, setPageNumber] = useState(1)
+    const { isLoading, isFetching, isError, error, data } = useQuery(
+        ['colors', pageNumber],
+        () => fetchColors(pageNumber),
+        {
+            keepPreviousData: true,
+        }
+    )
+    
+    if(isError) {
+        return <h2>{error.message}</h2>
+      }
+    if(isLoading) {
+    return <h2>Loading...</h2>
+    }
+
+  return (
+    <>
+     <div>
+         {data?.data.map((color) => {
+             return (
+                 <div key={color.id}>
+                     <h2>{color.id}. {color.label}</h2>
+                 </div>
+             )
+         })}
+     </div>
+
+     <div>
+         <button onClick={() => setPageNumber(page => page - 1)} disabled={pageNumber === 1}>prev page</button>
+         <button onClick={() => setPageNumber(page => page + 1)} disabled={pageNumber === 4}>next page</button>
+     </div>
+     {isFetching && 'loading'}
+    </>
+  )
+}
