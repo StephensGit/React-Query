6defcadcde73d39a9df1e710e70e9f7b2ca03209
Stephen Moran <stephenmoran@live.ie>
2022-03-20 09:43:17 +0000

    React Query Tutorial - 3 - Fetching Data with useQuery

---
+++ codevolution/react-query-starter/src/components/RQSuperHeroes.page.js
@@ -0,0 +1,21 @@
+import { useQuery } from "react-query"
+import axios from 'axios'
+
+const fetchSuperHeroes = () => {
+  return axios.get(`http://localhost:3000/superheroes`)
+}
+export const RQSuperHeroesPage = () => {
+  const { isLoading, data } =  useQuery('super-heroes', fetchSuperHeroes)
+
+  if(isLoading) {
+    return <h2>Loading...</h2>
+  }
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>
+      })}
+    </>
+  )
+}
